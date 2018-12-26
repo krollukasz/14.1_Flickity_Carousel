1b0bdb805e8153073c51a639ff2321e7184ba4a5
Łukasz Król <lionel@o2.pl>
2018-12-26 22:18:57 +0100

    Initialize carousel, set progress bar and run the reset button

---
+++ js/script.js
@@ -0,0 +1,43 @@
+"use strict";
+
+// Initialize carousel
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  cellAlign: 'left',
+  contain: true
+});
+
+// Progress bar
+var progressBar = document.querySelector('.progress-bar')
+
+flkty.on( 'scroll', function( progress ) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
+});
+
+
+// Photo description
+var flkty = new Flickity( '.main-carousel', {
+  imagesLoaded: true,
+  percentPosition: false
+});
+
+var caption = document.querySelector('.caption');
+
+flkty.on( 'select', function() {
+  caption.textContent = flkty.selectedElement.alt;
+});
+
+// Reset button
+var buttonGroup = document.querySelector('.button-group');
+var buttons = buttonGroup.querySelector('.restart-button');
+
+buttons = fizzyUIUtils.makeArray(buttons);
+buttonGroup.addEventListener('click', function (event) {
+  // filter for button clicks
+  if (!matchesSelector(event.target, '.restart-button')) {
+      return;
+  }
+  var index = buttons.indexOf(event.target);
+  flkty.select(index);
+});

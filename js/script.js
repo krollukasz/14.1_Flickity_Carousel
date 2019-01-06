12df0fcafdfcddfd5e06d6b0070c2db09e6be3ee
Łukasz Król <lionel@o2.pl>
2019-01-06 11:58:36 +0100

    Remove hash value from Flickity

--- js/script.js
+++ js/script.js
@@ -18,7 +18,6 @@ var elem = document.querySelector(".main-carousel");
 var flkty = new Flickity( elem, {
   cellAlign: "left",
   contain: true,
-  hash: true,
   imagesLoaded: true,
   percentPosition: false
 });
